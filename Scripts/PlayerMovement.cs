fc7172e99e544638d3f3808f754c6e4e8b35ac8f
AlexGama11 <52133865+AlexGama11@users.noreply.github.com>
2020-11-13 08:56:16 +0000

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Vector2 playerPos;
+    public float moveYaxis;
+    public float moveXaxis;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public float maxWidth;
+    public float minWidth;
+
+    private void Update()
+   {
+    
+		  
+    transform.position = Vector2.MoveTowards(transform.position, playerPos,speed*Time.deltaTime);
+	
+   {
+ 
+ if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+            {
+                playerPos = new Vector2(transform.position.x, transform.position.y + moveYaxis);
+            }
+   
+   else if(Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+            {
+                playerPos = new Vector2(transform.position.x, transform.position.y - moveYaxis);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.y > minWidth)
+            {
+                playerPos = new Vector2(transform.position.x, transform.position.y - moveXaxis);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.y > maxWidth)
+            {
+                playerPos = new Vector2(transform.position.x, transform.position.y + moveXaxis);
+            }
+
+        }
+   
+}
+}
